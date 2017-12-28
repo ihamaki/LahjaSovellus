2cc5343e720fd7ac65f1dafcc85e511e3a8a91db
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2017-12-28 23:23:16 +0200

    create database tables

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO GiftUser (username, password) VALUES ('MikkoMallikas', 'Mallikas');
+INSERT INTO GiftUser (username, password) VALUES ('Urpo', 'Turpo');
+
+INSERT INTO Person (name, birthday, description) VALUES ('Iines', '1990-02-10', 'Tykkää sisustamisesta');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,35 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE GiftUser(
+  id SERIAL PRIMARY KEY,
+  username varchar(30) NOT NULL,
+  password varchar(30) NOT NULL
+);
+
+CREATE TABLE Person(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  birthday DATE,
+  description varchar(500),
+  giftuser_id INTEGER REFERENCES GiftUser(id)
+);
+
+CREATE TABLE Gift(
+  id SERIAL PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  status boolean DEFAULT FALSE,
+  dateAdded DATE, 
+  description varchar(500),
+  giftuser_id INTEGER REFERENCES GiftUser(id),
+  person_id INTEGER REFERENCES Person(id)
+);
+
+CREATE TABLE Tag(
+  id SERIAL PRIMARY KEY,
+  name varchar(50)
+);
+
+CREATE TABLE GiftTag(
+  gift_id INTEGER REFERENCES Gift(id),
+  tag_id INTEGER REFERENCES Tag(id)
+);
